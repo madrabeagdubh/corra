27e8b53c2a3e9d68cdd3455b609a27a0a93eef23
macribo <roibeard.mac.unfraidh@gmail.com>
2026-02-25 16:21:17 +0000

    Commit before working on new sound feature for introModal.js

---
+++ js/game/systems/music/levelTunes.js
@@ -0,0 +1,13 @@
+// Level and scene music — separate from champion themes
+export const levelTunes = {
+  myLaganLove: `X: 1
+T: My Lagan Love
+R: air
+M: 3/4
+L: 1/8
+K: Gmaj
+|: G2 | B2 d2 B2 | A4 GA | B2 d2 g2 | e4 d2 |
+d2 B2 d2 | e4 d2 | B2 A2 G2 | A4 G2 |
+B2 d2 B2 | A4 GA | B2 d2 g2 | e4 d2 |
+d2 B2 A2 | G4 FG | A2 G2 F2 | G6 :|`,
+};

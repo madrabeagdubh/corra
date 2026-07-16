d25d2b69a18b49246c0ae0ef178018b090e21b6b
macribo <roibeard.mac.unfraidh@gmail.com>
2026-07-16 09:51:18 +0100

    remove py files.

--- inspect_d3sea.py
+++
@@ -1,8 +0,0 @@
-import json
-d = json.load(open('public/maps/bogMaps/d3_sea.json'))
-layer0 = d['layers'][0]
-LAND = {839, 840, 731}
-for y, row in enumerate(layer0):
-    cols = [x for x, g in enumerate(row) if g in LAND]
-    if cols:
-        print(f'row {y}: land at cols {min(cols)}-{max(cols)} ({len(cols)} tiles)')
